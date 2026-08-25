9bd3303d8b905f0a7859cf8e2f7238efaf080a6b
Khutwad-Bhavesh <b.vs.khutwad@gmail.com>
2026-08-25 19:33:15 +0530

    test: add automated diagnostic script for fish shell

---
+++ test_bionic.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+echo "=== Running Bionic Terminal Diagnostics ==="
+echo ""
+echo "If the script is working correctly, the following sentence will be bionic:"
+echo "The quick brown fox jumps over the lazy dog." | python3 ./bionic_filter.py
+echo ""
+echo "==========================================="
